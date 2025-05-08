47a84d9e7d63b2b475825e96bceedd8713e29770
Chris Meurink <cmeurink@cloudsuite.com>
2025-05-08 14:06:21 +0200

    fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   return (
     <HydrateClient>
       <main className="flex grow flex-col items-center pt-8">
-        <h1 className="font-bold text-3xl">CloudSuite Demo's Headless Interpretation</h1>
+        <h1 className="font-bold text-3xl">{`CloudSuite Demo's Headless Interpretation`}</h1>
       </main>
     </HydrateClient>
   );
